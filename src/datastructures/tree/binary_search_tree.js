229420b6f9c2ca2ce261c5933e3974433af983ce
George Sun <sunwinner3@gmail.com>
2015-07-13 22:31:44 +0800

    BST impl in progress.

--- src/datastructures/tree/binary_search_tree.js
+++ src/datastructures/tree/binary_search_tree.js
@@ -21,6 +21,74 @@ function sizeOfNode(node) {
     return node.num;
 }
 
+function insertNode(node, newNode) {
+    if (newNode.key < node.key) {
+        if (node.left === null) {
+            node.left = newNode;
+        } else {
+            insertNode(node.left, newNode);
+        }
+    } else {
+        if (node.right === null) {
+            node.right = newNode;
+        } else {
+            insertNode(node.right, newNode);
+        }
+    }
+    node.num = 1 + sizeOfNode(node.left) + sizeOfNode(node.right);
+}
+
+function searchNode(node, key) {
+    if (node === null) {
+        return false;
+    }
+
+    if (key < node.key) {
+        return searchNode(node.left, key);
+    } else if (key > node.key) {
+        return searchNode(node.right, key);
+    } else {
+        return true;
+    }
+}
+
+function removeNode(node, key) {
+    if (node === null) {
+        return null;
+    }
+
+    if (key < node.key) {
+        node.left = removeNode(node.left, key);
+    } else if (key > node.key) {
+        node.right = removeNode(node.right, key);
+    } else {
+        if (node.left === null && node.rith === null) {
+            node = null;
+            return node;
+        }
+
+        if (node.left === null) {
+            return node.right;
+        } else if (node.right === null) {
+            return node.left;
+        }
+
+        var aux = findMinNode(node.right);
+        node.key = aux.key;
+        node.right = removeNode(node.right, aux.key);
+    }
+
+    node.num = sizeOfNode(node.left) + sizeOfNode(node.right) + 1;
+    return node;
+}
+
+function findMinNode(node) {
+    while (node && node.left !== null) {
+        node = node.left;
+    }
+    return node;
+}
+
 BinarySearchTree.prototype.size = function() {
     return sizeOfNode(this.root);
 };
@@ -29,9 +97,19 @@ BinarySearchTree.prototype.empty = function() {
     return this.size() === 0;
 };
 
-BinarySearchTree.prototype.insert = function(key) {};
+BinarySearchTree.prototype.insert = function(key) {
+    var node = new Node(key, 1);
+    if (this.root === null) {
+        this.root = node;
+        this.root.num = 1;
+    } else {
+        insertNode(this.root, node);
+    }
+};
 
-BinarySearchTree.prototype.search = function(key) {};
+BinarySearchTree.prototype.search = function(key) {
+    return searchNode(this.root, key);
+};
 
 BinarySearchTree.prototype.inOrderTraverse = function() {};
 
@@ -43,7 +121,9 @@ BinarySearchTree.prototype.min = function() {};
 
 BinarySearchTree.prototype.max = function() {};
 
-BinarySearchTree.prototype.remove = function(key) {};
+BinarySearchTree.prototype.remove = function(key) {
+    this.root = removeNode(this.root, key);
+};
 
 BinarySearchTree.prototype.height = function() {};
 
--- test/datastructures/tree/tree_spec.js
+++ test/datastructures/tree/tree_spec.js
@@ -14,4 +14,62 @@ describe("Test Binary Search Tree", function() {
         expect(bst.size()).to.equal(0);
         expect(bst.empty()).to.equal(true);
     });
+
+    it("should not be empty with elements added", function() {
+        bst.insert("a");
+        expect(bst.size()).to.equal(1);
+        expect(bst.empty()).to.equal(false);
+
+        bst.insert("b");
+        expect(bst.size()).to.equal(2);
+        expect(bst.empty()).to.equal(false);
+
+        bst.insert("b");
+        expect(bst.size()).to.equal(3);
+        expect(bst.empty()).to.equal(false);
+    });
+
+    it("should contains the elements been added", function() {
+        bst.insert("a");
+        bst.insert("b");
+
+        expect(bst.search("a")).to.equal(true);
+        expect(bst.search("b")).to.equal(true);
+        expect(bst.search("c")).to.equal(false);
+    });
+
+    it("should remove the specified element", function() {
+
+        bst.insert("c");
+        bst.insert("a");
+        bst.insert("a");
+        bst.insert("b");
+        bst.insert("d");
+
+        bst.remove("a");
+        expect(bst.search("a")).to.equal(true);
+        expect(bst.search("b")).to.equal(true);
+        expect(bst.size()).to.equal(4);
+
+        bst.remove("a");
+        expect(bst.search("a")).to.equal(false);
+        expect(bst.search("b")).to.equal(true);
+        expect(bst.size()).to.equal(3);
+
+        bst.remove("a");
+        expect(bst.size()).to.equal(3);
+        expect(bst.search("b")).to.equal(true);
+
+        bst.remove("b");
+        expect(bst.search("b")).to.equal(false);
+        expect(bst.size()).to.equal(2);
+
+        bst.remove("c");
+        expect(bst.search("c")).to.equal(false);
+        expect(bst.size()).to.equal(1);
+
+        bst.remove("d");
+        expect(bst.search("d")).to.equal(false);
+        expect(bst.size()).to.equal(0);
+    });
 });
