816d3d51751192ef7d1096adc4dd25402008c9aa
George Sun <sunwinner3@gamil.com>
2015-07-14 16:14:37 +0800

    Change mocha reporter.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
         mochaTest: {
             test: {
                 options: {
-                    reporter: 'spec',
+                    reporter: 'dot',
                     captureFile: 'results.txt', // Optionally capture the reporter output to a file
                     quiet: false, // Optionally suppress output to standard out (defaults to false)
                     clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
