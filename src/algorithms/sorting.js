8af9eaf87c5163f8acdf86dbffd34c1189e1fd74
George Sun <sunwinner3@gmail.com>
2015-07-14 23:15:19 +0800

    Adding insertion sort, selection sort, and merge sort.

--- src/algorithms/sorting.js
+++ src/algorithms/sorting.js
@@ -6,6 +6,46 @@ function swap(ary, i, j) {
     ary[j] = tmp;
 }
 
+function merge_sort_rec(ary) {
+    if (!ary) {
+        return null;
+    }
+
+    var len = ary.length,
+        mid = Math.floor(len / 2),
+        left, right;
+
+    if (len === 0 || len === 1) {
+        return ary;
+    }
+
+    left = ary.slice(0, mid);
+    right = ary.slice(mid, len);
+    return merge(merge_sort_rec(left), merge_sort_rec(right));
+}
+
+function merge(ary1, ary2) {
+    var result = [],
+        i1 = 0,
+        i2 = 0;
+    while (i1 < ary1.length && i2 < ary2.length) {
+        if (ary1[i1] < ary2[i2]) {
+            result.push(ary1[i1++]);
+        } else {
+            result.push(ary2[i2++]);
+        }
+    }
+
+    while (i1 < ary1.length) {
+        result.push(ary1[i1++]);
+    }
+
+    while (i2 < ary2.length) {
+        result.push(ary2[i2++]);
+    }
+    return result;
+}
+
 var Sorting = {
     bobble_sort: function(ary) {
         if (!ary) {
@@ -23,6 +63,52 @@ var Sorting = {
         }
         return ary;
     },
+
+    selection_sort: function(ary) {
+        if (!ary) {
+            return null;
+        }
+
+        var len = ary.length,
+            indexMin, i, j;
+        for (i = 0; i < len - 1; i++) {
+            indexMin = i;
+            for (j = i; j < len; j++) {
+                if (ary[indexMin] > ary[j]) {
+                    indexMin = j;
+                }
+            }
+
+            if (i !== indexMin) {
+                swap(ary, i, indexMin);
+            }
+        }
+        return ary;
+    },
+
+    insertion_sort: function(ary) {
+        if (!ary) {
+            return null;
+        }
+
+        var len = ary.length,
+            i, j, tmp;
+
+        for (i = 1; i < len; i++) {
+            j = i;
+            tmp = ary[i];
+            while (j > 0 && ary[j - 1] > tmp) {
+                ary[j] = ary[j - 1];
+                j -= 1;
+            }
+            ary[j] = tmp;
+        }
+        return ary;
+    },
+
+    merge_sort: function(ary) {
+        return merge_sort_rec(ary);
+    }
 };
 
 module.exports = Sorting;
--- test/algorithms/sorting_spec.js
+++ test/algorithms/sorting_spec.js
@@ -14,4 +14,34 @@ describe("Test sorting algorithms", function() {
             expect(Sorting.bobble_sort([2, 1])).to.eql([1, 2]);
         });
     });
+
+    describe("Test selection sort", function() {
+        it("should sort the given elements", function() {
+            expect(Sorting.selection_sort(null)).to.eql(null);
+            expect(Sorting.selection_sort([])).to.eql([]);
+            expect(Sorting.selection_sort([1])).to.eql([1]);
+            expect(Sorting.selection_sort([1, 2])).to.eql([1, 2]);
+            expect(Sorting.selection_sort([2, 1])).to.eql([1, 2]);
+        });
+    });
+
+    describe("Test insertion sort", function() {
+        it("should sort the given elements", function() {
+            expect(Sorting.insertion_sort(null)).to.eql(null);
+            expect(Sorting.insertion_sort([])).to.eql([]);
+            expect(Sorting.insertion_sort([1])).to.eql([1]);
+            expect(Sorting.insertion_sort([1, 2])).to.eql([1, 2]);
+            expect(Sorting.insertion_sort([2, 1])).to.eql([1, 2]);
+        });
+    });
+
+    describe("Test merge sort", function() {
+        it("should sort the given elements", function() {
+            expect(Sorting.merge_sort(null)).to.eql(null);
+            expect(Sorting.merge_sort([])).to.eql([]);
+            expect(Sorting.merge_sort([1])).to.eql([1]);
+            expect(Sorting.merge_sort([1, 2])).to.eql([1, 2]);
+            expect(Sorting.merge_sort([2, 1])).to.eql([1, 2]);
+        });
+    });
 });
