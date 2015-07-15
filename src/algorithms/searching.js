d6e06c8f826b05858817a9c222000ead58cb15fa
George Sun <sunwinner3@gmail.com>
2015-07-15 08:24:50 +0800

    Finish sequential searching.

--- src/algorithms/searching.js
+++ src/algorithms/searching.js
@@ -2,6 +2,18 @@
 
 var search = {
     sequentialSearch: function(ary, e) {
+        if (!ary) {
+            return null;
+        }
 
+        var index, len;
+        for (index = 0, len = ary.length; index < len; index++) {
+            if (ary[index] === e) {
+                return index;
+            }
+        }
+        return -1;
     }
 };
+
+module.exports = search;
--- test/algorithms/searching_spec.js
+++ test/algorithms/searching_spec.js
@@ -5,8 +5,20 @@ var search = require("../../src/algorithms/searching"),
 
 describe("Test searching", function() {
     describe("test sequential searching", function() {
+        it("should return null for null", function() {
+            expect(search.sequentialSearch(null, null)).to.equal(null);
+        });
+
+        it("should return -1 for empty collection", function() {
+            expect(search.sequentialSearch([], "a")).to.equal(-1);
+        });
+
+        it("should return -1 for non-existing element", function() {
+            expect(search.sequentialSearch(["b", "c"], "a")).to.equal(-1);
+        });
+
         it("should return the element been searching", function() {
-            // TODO
+            expect(search.sequentialSearch(["b", "c"], "c")).to.equal(1);
         });
     });
 });
