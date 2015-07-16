5fb5cee4c0bae0722036ff27853d6478684c500c
George Sun <sunwinner3@gmail.com>
2015-07-17 07:19:39 +0800

    Revise graph tests.

--- test/datastructures/graph/graph_spec.js
+++ test/datastructures/graph/graph_spec.js
@@ -36,7 +36,7 @@ function addNodesTo(graph, addEdges) {
         graph.addEdge("5", "2", 9);
         graph.addEdge("6", "3", 9);
         graph.addEdge("6", "6", 9);
-        return expect(graph.edgeSize).to.equal(initEdgeSize + 8);
+        expect(graph.edgeSize).to.equal(initEdgeSize + 8);
     }
 }
 
@@ -46,20 +46,20 @@ describe("Test Graph", function() {
         var graph = new Graph();
         it("should have 0 edge and 0 node initially", function() {
             expect(graph.nodeSize).to.equal(0);
-            return expect(graph.edgeSize).to.equal(0);
+            expect(graph.edgeSize).to.equal(0);
         });
         it("should return the node object added, or undefined if the id exists", function() {
             expect((graph.addNode("item")) instanceof Object).to.equal(true);
             expect((graph.addNode("1")) instanceof Object).to.equal(true);
-            return expect((graph.addNode(null)) instanceof Object).to.equal(true);
+            expect((graph.addNode(null)) instanceof Object).to.equal(true);
         });
         it("should return undefined if the node id already exists", function() {
             expect(graph.addNode("item")).to.equal(undefined);
             expect(graph.addNode("1")).to.equal(undefined);
-            return expect(graph.addNode(null)).to.equal(undefined);
+            expect(graph.addNode(null)).to.equal(undefined);
         });
-        return it("should have kept the node size constant with non-insertions", function() {
-            return expect(graph.nodeSize).to.equal(3);
+        it("should have kept the node size constant with non-insertions", function() {
+            expect(graph.nodeSize).to.equal(3);
         });
     });
 
@@ -69,19 +69,19 @@ describe("Test Graph", function() {
         it("should return undefined if the node's not found", function() {
             expect(graph.getNode(null)).to.equal(undefined);
             expect(graph.getNode(void 0)).to.equal(undefined);
-            return expect(graph.getNode(2)).to.equal(undefined);
+            expect(graph.getNode(2)).to.equal(undefined);
         });
         it("should return the added node", function() {
             addNodesTo(graph);
             expect((graph.getNode("1")) instanceof Object).to.equal(true);
             expect((graph.getNode("2")) instanceof Object).to.equal(true);
-            return expect((graph.getNode("6")) instanceof Object).to.equal(true);
+            expect((graph.getNode("6")) instanceof Object).to.equal(true);
         });
-        return it("should return a defined object if null and undefined are found", function() {
+        it("should return a defined object if null and undefined are found", function() {
             graph.addNode(null);
             graph.addNode(void 0);
             expect((graph.getNode(null)) instanceof Object).to.equal(true);
-            return expect((graph.getNode(void 0)) instanceof Object).to.equal(true);
+            expect((graph.getNode(void 0)) instanceof Object).to.equal(true);
         });
     });
 
@@ -89,25 +89,25 @@ describe("Test Graph", function() {
         var graph = new Graph();
         it("should return undefined if the node doesn't exist in the first place", function() {
             expect(graph.removeNode(null)).to.equal(undefined);
-            return expect(graph.removeNode(2)).to.equal(undefined);
+            expect(graph.removeNode(2)).to.equal(undefined);
         });
 
         it("should have kept the node size constant", function() {
-            return expect(graph.nodeSize).to.equal(0);
+            expect(graph.nodeSize).to.equal(0);
         });
         it("should return the value of node removed", function() {
             addNodesTo(graph);
             expect((graph.removeNode("1")) instanceof Object).to.equal(true);
             expect((graph.removeNode("3")) instanceof Object).to.equal(true);
-            return expect((graph.removeNode("6")) instanceof Object).to.equal(true);
+            expect((graph.removeNode("6")) instanceof Object).to.equal(true);
         });
         it("should have updated the node size", function() {
-            return expect(graph.nodeSize).to.equal(3);
+            expect(graph.nodeSize).to.equal(3);
         });
-        return it("should have removed the node", function() {
+        it("should have removed the node", function() {
             expect(graph.getNode("1")).to.equal(undefined);
             expect(graph.getNode("3")).to.equal(undefined);
-            return expect(graph.getNode("6")).to.equal(undefined);
+            expect(graph.getNode("6")).to.equal(undefined);
         });
     });
 
@@ -142,17 +142,17 @@ describe("Test Graph", function() {
 
         it("should allow the node to add an edge to itself", function() {
             expect((graph.addEdge("2", "2")) instanceof Object).to.equal(true);
-            return expect((graph.addEdge("6", "6")) instanceof Object).to.equal(true);
+            expect((graph.addEdge("6", "6")) instanceof Object).to.equal(true);
         });
 
         it("should count a self-directing edge as a single one", function() {
-            return expect(graph.edgeSize).to.equal(8);
+            expect(graph.edgeSize).to.equal(8);
         });
 
-        return it("should return undefined if the edge already exists", function() {
+        it("should return undefined if the edge already exists", function() {
             expect(graph.addEdge("1", "2")).to.equal(undefined);
             expect(graph.addEdge("2", "2")).to.equal(undefined);
-            return expect(graph.addEdge("2", "1")).to.equal(undefined);
+            expect(graph.addEdge("2", "1")).to.equal(undefined);
         });
     });
 
@@ -160,14 +160,14 @@ describe("Test Graph", function() {
         var graph;
         graph = new Graph();
         it("should return undefined if the nodes aren't found", function() {
-            return expect(graph.getEdge("1", "2")).to.equal(undefined);
+            expect(graph.getEdge("1", "2")).to.equal(undefined);
         });
         it("should return undefined if the edge isn't found", function() {
             addNodesTo(graph, true);
             expect(graph.getEdge("3", "5")).to.equal(undefined);
-            return expect(graph.getEdge("1", "2")).to.equal(undefined);
+            expect(graph.getEdge("1", "2")).to.equal(undefined);
         });
-        return it("should return the edge found", function() {
+        it("should return the edge found", function() {
             expect((graph.getEdge("1", "4")) instanceof Object).to.equal(true);
             expect((graph.getEdge("2", "1")) instanceof Object).to.equal(true);
             expect((graph.getEdge("2", "3")) instanceof Object).to.equal(true);
@@ -175,7 +175,7 @@ describe("Test Graph", function() {
             expect((graph.getEdge("5", "1")) instanceof Object).to.equal(true);
             expect((graph.getEdge("5", "2")) instanceof Object).to.equal(true);
             expect((graph.getEdge("6", "3")) instanceof Object).to.equal(true);
-            return expect((graph.getEdge("6", "6")) instanceof Object).to.equal(true);
+            expect((graph.getEdge("6", "6")) instanceof Object).to.equal(true);
         });
     });
 
@@ -184,11 +184,11 @@ describe("Test Graph", function() {
 
         it("should return undefined if either node's not found", function() {
             expect(graph.removeEdge(1, 2)).to.equal(undefined);
-            return expect(graph.removeEdge(void 0, void 0)).to.equal(undefined);
+            expect(graph.removeEdge(void 0, void 0)).to.equal(undefined);
         });
 
         it("should have kept the edge count at 0", function() {
-            return expect(graph.edgeSize).to.equal(0);
+            expect(graph.edgeSize).to.equal(0);
         });
 
         it("should return undefined if the edge doesn't exist", function() {
@@ -206,19 +206,19 @@ describe("Test Graph", function() {
             expect(graph.removeEdge("3", "2").weight).to.equal(9);
             expect(graph.removeEdge("5", "1").weight).to.equal(9);
             expect(graph.removeEdge("5", "2").weight).to.equal(9);
-            return expect(graph.removeEdge("6", "3").weight).to.equal(9);
+            expect(graph.removeEdge("6", "3").weight).to.equal(9);
         });
 
         it("should have kept track of the edge count", function() {
-            return expect(graph.edgeSize).to.equal(1);
+            expect(graph.edgeSize).to.equal(1);
         });
 
         it("should remove a self-directing correctly", function() {
             expect(graph.removeEdge("6", "6").weight).to.equal(9);
-            return expect(graph.edgeSize).to.equal(0);
+            expect(graph.edgeSize).to.equal(0);
         });
 
-        return it("should leave an empty graph after removing all the edges", function() {
+        it("should leave an empty graph after removing all the edges", function() {
             expect(graph.removeEdge("1", "4")).to.equal(undefined);
             expect(graph.removeEdge("2", "1")).to.equal(undefined);
             expect(graph.removeEdge("2", "3")).to.equal(undefined);
@@ -226,7 +226,7 @@ describe("Test Graph", function() {
             expect(graph.removeEdge("5", "1")).to.equal(undefined);
             expect(graph.removeEdge("5", "2")).to.equal(undefined);
             expect(graph.removeEdge("6", "3")).to.equal(undefined);
-            return expect(graph.removeEdge("6", "6")).to.equal(undefined);
+            expect(graph.removeEdge("6", "6")).to.equal(undefined);
         });
     });
 
@@ -236,14 +236,14 @@ describe("Test Graph", function() {
 
         it("should return empty array for a non-existant node", function() {
             expect(graph.getOutEdgesOf("6")).to.eql([]);
-            return expect(graph.getOutEdgesOf(void 0)).to.eql([]);
+            expect(graph.getOutEdgesOf(void 0)).to.eql([]);
         });
 
         it("should return empty array for no edges", function() {
             addNodesTo(graph);
             expect(graph.getInEdgesOf("1")).to.eql([]);
             expect(graph.getInEdgesOf("2")).to.eql([]);
-            return expect(graph.getInEdgesOf("6")).to.eql([]);
+            expect(graph.getInEdgesOf("6")).to.eql([]);
         });
 
         return it("should return the in edges", function() {
@@ -268,7 +268,7 @@ describe("Test Graph", function() {
             expect(graph2.getInEdgesOf("4")).to.contain(graph2.getEdge("1", "4"));
             expect(graph2.getInEdgesOf("5")).to.eql([]);
             expect(graph2.getInEdgesOf("6").length).to.equal(1);
-            return expect(graph2.getInEdgesOf("6")).to.contain(graph2.getEdge("6", "6"));
+            expect(graph2.getInEdgesOf("6")).to.contain(graph2.getEdge("6", "6"));
         });
     });
 
@@ -278,16 +278,16 @@ describe("Test Graph", function() {
 
         it("should return empty array for a non-existant node", function() {
             expect(graph.getOutEdgesOf("6")).to.eql([]);
-            return expect(graph.getOutEdgesOf(void 0)).to.eql([]);
+            expect(graph.getOutEdgesOf(void 0)).to.eql([]);
         });
 
         it("should return empty array for no edges", function() {
             addNodesTo(graph);
             expect(graph.getOutEdgesOf("1")).to.eql([]);
-            return expect(graph.getOutEdgesOf("2")).to.eql([]);
+            expect(graph.getOutEdgesOf("2")).to.eql([]);
         });
 
-        return it("should return the in edges", function() {
+        it("should return the in edges", function() {
             addNodesTo(graph2, true);
 
             /*
@@ -309,7 +309,7 @@ describe("Test Graph", function() {
             expect(graph2.getOutEdgesOf("5")).to.contain(graph2.getEdge("5", "2"));
             expect(graph2.getOutEdgesOf("6").length).to.equal(2);
             expect(graph2.getOutEdgesOf("6")).to.contain(graph2.getEdge("6", "3"));
-            return expect(graph2.getOutEdgesOf("6")).to.contain(graph2.getEdge("6", "6"));
+            expect(graph2.getOutEdgesOf("6")).to.contain(graph2.getEdge("6", "6"));
         });
     });
 
@@ -319,14 +319,14 @@ describe("Test Graph", function() {
 
         it("should return an empty array if node doesn't exist", function() {
             expect(graph.getAllEdgesOf(1)).to.eql([]);
-            return expect(graph.getAllEdgesOf(void 0)).to.eql([]);
+            expect(graph.getAllEdgesOf(void 0)).to.eql([]);
         });
 
         it("should return an empty array if the node doesn't have edges", function() {
             addNodesTo(graph);
             expect(graph.getAllEdgesOf("1")).to.eql([]);
             expect(graph.getAllEdgesOf("2")).to.eql([]);
-            return expect(graph.getAllEdgesOf("6")).to.eql([]);
+            expect(graph.getAllEdgesOf("6")).to.eql([]);
         });
 
         it("should return an array of edges", function() {
@@ -355,13 +355,13 @@ describe("Test Graph", function() {
             expect(graph2.getAllEdgesOf("4")).to.contain(graph2.getEdge("1", "4"));
             expect(graph2.getAllEdgesOf("5").length).to.equal(2);
             expect(graph2.getAllEdgesOf("5")).to.contain(graph2.getEdge("5", "1"));
-            return expect(graph2.getAllEdgesOf("5")).to.contain(graph2.getEdge("5", "2"));
+            expect(graph2.getAllEdgesOf("5")).to.contain(graph2.getEdge("5", "2"));
         });
 
         it("should not duplicate a self-pointing edge", function() {
             expect(graph2.getAllEdgesOf("6").length).to.equal(2);
             expect(graph2.getAllEdgesOf("6")).to.contain(graph2.getEdge("6", "3"));
-            return expect(graph2.getAllEdgesOf("6")).to.contain(graph2.getEdge("6", "6"));
+            expect(graph2.getAllEdgesOf("6")).to.contain(graph2.getEdge("6", "6"));
         });
     });
 
@@ -379,10 +379,10 @@ describe("Test Graph", function() {
             var callback = sinon.spy();
             addNodesTo(graph);
             graph.forEachNode(callback);
-            return expect(callback.callCount).to.equal(6);
+            expect(callback.callCount).to.equal(6);
         });
 
-        return it("should pass nodeObject and nodeId to the callback", function() {
+        it("should pass nodeObject and nodeId to the callback", function() {
             var callback = sinon.spy();
             graph.forEachNode(callback);
             expect(callback.lastCall.args.length).to.equal(2);
@@ -404,15 +404,15 @@ describe("Test Graph", function() {
             var callback = sinon.spy();
             addNodesTo(graph, true);
             graph.forEachEdge(callback);
-            return expect(callback.callCount).to.equal(8);
+            expect(callback.callCount).to.equal(8);
         });
 
-        return it("should reach the isolated node with an edge toward itself", function() {
+        it("should reach the isolated node with an edge toward itself", function() {
             var callback = sinon.spy();
             graph.addNode("99");
             graph.addEdge("99", "99", 999);
             graph.forEachEdge(callback);
-            return expect(callback.callCount).to.equal(9);
+            expect(callback.callCount).to.equal(9);
         });
     });
 });
